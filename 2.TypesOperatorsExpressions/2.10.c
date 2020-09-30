7e2a6e5482a220895f237ee9fe9ef34d87715cfb
trluxus <trluxus@protonmail.com>
2020-09-30 12:29:58 +0300

    Learning_C. Section 2.11.
    Signed-off-by: trluxus <trluxus@protonmail.com>

--- 2.TypesOperatorsExpressions/2.10.c
+++ 2.TypesOperatorsExpressions/2.10.c
@@ -1,3 +1,26 @@
 // Exercise 2-10. Rewrite the function lower, which converts upper case letters
 // to lower case, with a conditional expression instead of if-else.
 
+#include <stdio.h>
+
+int lower(int c);
+
+int main()
+{
+    int i;
+
+    char s[] = "TEST\0";
+    printf("%s\n", s);
+
+    printf("lower s = ");
+    for (i = 0; s[i] != '\0'; i++)
+        printf("%c", lower(s[i]));
+    printf("\n");
+
+    return 0;
+}
+
+int lower(int c)
+{
+    return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c;
+}
